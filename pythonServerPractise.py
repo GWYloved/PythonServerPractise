a01ab21d6b60e235ff66fe52c88a585b1d43f3d0
GWYloved <913925047@qq.com>
2018-03-15 22:27:07 +0800

    实现put方式

--- pythonServerPractise.py
+++ pythonServerPractise.py
@@ -1,16 +1,21 @@
-from flask import Flask
-import flask_restful
-
+from flask import Flask,request
+from flask_restful import Resource,Api
+# 以上为引用，from是从哪里，import是导入，必须要from import
 
 app = Flask(__name__)
-api = flask_restful.Api(app)
+api = Api(app)
 
+todos = {}
 # @app.route('/')
-class HelloWorld(flask_restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
+class HelloWorld(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
 
-api.add_resource(HelloWorld, '/')
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(HelloWorld, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
